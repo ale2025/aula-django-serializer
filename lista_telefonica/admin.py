49d9f867df1404eb1346aa490487cfc8e89e549a
Alexandre <alexandrecardosodebarros1@outlook.com>
2020-01-20 16:47:29 -0300

    criei crud de anuncio

--- lista_telefonica/admin.py
+++ lista_telefonica/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from lista_telefonica.models import Contato
 
+admin.site.register(Contato)
 # Register your models here.
---
+++ lista_telefonica/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from lista_telefonica.models import Contato, Anuncio
+
+class ContatoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contato
+        fields =['nome','telefone','email']
+        
+
+class AnuncioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Anuncio        
+        fields = ['nome_da_empresa','descricao','telefone_de_contato','valor']
